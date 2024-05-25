a771bc0f493569bd00e3823b89fa3aefdf72bc13
Ricky Huang <rickypc@users.noreply.github.com>
2024-05-24 21:20:00 -0700

    Use new data structure with phrase(s) components.

---
+++ docs/buddhism/phrases/_dependent_origination.js
@@ -0,0 +1,23 @@
+/*!
+ * All the code that follow is
+ * Copyright (c) 2015 - 2024 Richard Huang <rickypc@users.noreply.github.com>.
+ * All Rights Reserved. Not for reuse without permission.
+ */
+
+export default {
+  sanskrit: {
+    children: 'ये धर्मा हेतु प्रभवा हेतुं तेषां तथागतः ह्यवदत् तेषां च यो निरोध एवं वादी महाश्रमणः',
+    title: 'प्रतीत्यसमुत्पाद',
+  },
+  tibetan: {
+    children: 'ཡེ་དྷརྨ་ཧེ་ཏུ་པྲ་བྷ་ཝཱ་ཧེ་ཏུནྟེ་ཥཱནྟ་ཐཱ་ག་ཏཔོ་ཧྱ་ཝ་དཏ། ཏེ་ཥཱཉྩ་ཡཔོ་ནི་རཔོ་དྷ་ཨེ་ཝཾ་བཱ་དཱི་མ་ཧཱ་ཤྲ་མ་ཎཨཡེ་སྭཱཧཱ',
+    repeat: 40,
+    title: 'རྟེན་ཅིང་འབྲེལ་བར་འབྱུང་བ་',
+  },
+  transliteration: {
+    children: 'ye dhammā hetuppabhavā tesaṁ hetuṁ tathāgato āha । tesaṃ ca yo nirodho evaṁvādī mahāsamaṇo',
+    repetition: 7,
+    speech: 'y dhamma hei tu pabawa t samm hei tum tathagato aha, t samm ca yo nirodho ewam wadi maha samano.',
+    title: 'Dependent Origination',
+  },
+};

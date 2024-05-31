59db3fc8ac35c5c49dab95200fe053c245cdb1c7
Ricky Huang <rickypc@users.noreply.github.com>
2024-05-31 04:11:04 -0700

    Adjust the phrase and speech components to support more features.

--- src/components/common/Phrase/index.jsx
+++ src/components/common/Phrase/index.jsx
@@ -7,12 +7,11 @@
 import CodeBlock from '@theme/CodeBlock';
 import { GenIcon } from 'react-icons/lib';
 import Image from '@site/src/components/common/Image';
-import { key } from '@site/src/data/common';
+import { clsx, key } from '@site/src/data/common';
 import Link from '@site/src/components/common/Link';
 import MDXDetails from '@theme-original/MDXComponents/Details';
 import { memo } from 'react';
 import PropTypes from 'prop-types';
-import Repetition from '@site/src/components/common/Repetition';
 import Speech from '@site/src/components/common/Speech';
 // eslint-disable-next-line import/extensions
 import pdf from '#buddhism/_pdf.js';
@@ -21,6 +20,83 @@ import styles from './styles.module.css';
 function FaScroll(props) {
   return GenIcon({ tag: 'svg', attr: { viewBox: '0 0 576 512' }, child: [{ tag: 'path', attr: { d: 'M0 80v48c0 17.7 14.3 32 32 32H48 96V80c0-26.5-21.5-48-48-48S0 53.5 0 80zM112 32c10 13.4 16 30 16 48V384c0 35.3 28.7 64 64 64s64-28.7 64-64v-5.3c0-32.4 26.3-58.7 58.7-58.7H480V128c0-53-43-96-96-96H112zM464 480c61.9 0 112-50.1 112-112c0-8.8-7.2-16-16-16H314.7c-14.7 0-26.7 11.9-26.7 26.7V384c0 53-43 96-96 96H368h96z' }, child: [] }] })(props);
 }
+
+const Instruction = memo(function Instruction({ image, text, transliteration }) {
+  if (image) {
+    return (
+      <MDXDetails>
+        <summary>{text || transliteration.title}</summary>
+        <Image
+          alt={transliteration.title}
+          className={styles.picture}
+          picture={{ fallback: image }}
+        />
+      </MDXDetails>
+    );
+  }
+  if (text) {
+    return <div className={styles.instruction}>{text}</div>;
+  }
+});
+Instruction.propTypes = {
+  image: PropTypes.string,
+  text: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+    PropTypes.string,
+  ]),
+  transliteration: PropTypes.shape(),
+};
+
+const Repetition = memo(Object.assign(function Repetition({ value = 1 }) {
+  if (value < 2) {
+    return null;
+  }
+  return (
+    <span
+      className={clsx(styles.badge, 'badge', 'badge--primary')}
+      title={`Preferred repetition: ${value} times`}
+    >
+      {`${value}x`}
+    </span>
+  );
+}, {
+  propTypes: {
+    value: PropTypes.number,
+  },
+}));
+
+const Support = memo(function Support({ repetition = 0, transliteration }) {
+  if (
+    !repetition
+    && !Object.prototype.hasOwnProperty.call(pdf, transliteration?.pdf)
+    && !transliteration.repetition
+    && !transliteration.speech
+  ) {
+    return null;
+  }
+  return (
+    <div className={styles.support}>
+      <Repetition value={repetition || transliteration.repetition} />
+      {Object.prototype.hasOwnProperty.call(pdf, transliteration?.pdf) && (
+        <Link
+          className={styles.roll}
+          href={key(transliteration.pdf, '/pdf', '/', 'pdf', '.')}
+          rel="noopener noreferrer"
+          target="_blank"
+          title={`Open ${transliteration.title} mantra roll`}
+        >
+          <FaScroll />
+        </Link>
+      )}
+      <Speech>{transliteration.speech}</Speech>
+    </div>
+  );
+});
+Support.propTypes = {
+  repetition: PropTypes.number,
+  transliteration: PropTypes.shape(),
+};
 
 export default memo(Object.assign(function Phrase({
   image,
@@ -33,35 +109,13 @@ export default memo(Object.assign(function Phrase({
   }
   return (
     <>
-      {(instruction || image) && (
-        <MDXDetails>
-          <summary>{instruction || transliteration.title}</summary>
-          {image && (
-            <Image
-              alt={transliteration.title}
-              className={styles.picture}
-              picture={{ fallback: image }}
-            />
-          )}
-        </MDXDetails>
-      )}
-      <CodeBlock>
+      <Instruction image={image} text={instruction} transliteration={transliteration} />
+      <CodeBlock className={styles.phrase} language="js">
+        {transliteration.className && `// ${transliteration.className}\n`}
         {transliteration.children}
         ॥
       </CodeBlock>
-      <Repetition value={repetition || transliteration.repetition} />
-      {Object.prototype.hasOwnProperty.call(pdf, transliteration?.pdf) && (
-        <Link
-          className={styles.roll}
-          href={key(transliteration.title, '/pdf', '/', 'pdf', '.')}
-          rel="noopener noreferrer"
-          target="_blank"
-          title={`Open ${transliteration.title} mantra roll`}
-        >
-          <FaScroll />
-        </Link>
-      )}
-      <Speech>{transliteration.speech}</Speech>
+      <Support repetition={repetition} transliteration={transliteration} />
     </>
   );
 }, {

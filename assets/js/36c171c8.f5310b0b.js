a70ed15a14fbfd055bdd95a5fd33d67b6cac32e8
Ricky Huang <rickypc@users.noreply.github.com>
2024-06-01 02:33:26 -0700

    Deploy website - based on fd7334866500a76e7727b667909f8281b78447b9

---
+++ assets/js/36c171c8.f5310b0b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrickypc_github_io=self.webpackChunkrickypc_github_io||[]).push([[393],{275:s=>{s.exports=JSON.parse('{"tags":[{"label":"Buddhism","permalink":"/docs/tags/buddhism","count":3}]}')}}]);

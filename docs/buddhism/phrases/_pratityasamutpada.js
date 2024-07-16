b968bb423292795a8a9df3238e58981fcd1e2206
Ricky Huang <rickypc@users.noreply.github.com>
2024-07-15 19:39:40 -0700

    Adjust phrase.

--- docs/buddhism/phrases/_pratityasamutpada.js
+++ docs/buddhism/phrases/_pratityasamutpada.js
@@ -7,11 +7,15 @@
 export default {
   pali: {
     children: 'ye dhammā hetuppabhavā hetuṁ tesaṁ tathāgato uvāca । tesañ-ca yo nirodho evaṁvādī mahāsamaṇo । aññāṇaṁ cīyate kammaṁ jananaṁ kammakāraṇaṁ । ñāṇaṁ na cīyate kammaṁ kammābhāvaṁ na jāyate',
+    sinhala: {
+      children: 'යේ ධර්මා හේතුප්‍රභවා හේතුං තේෂාං තථාගත උවාච । තේෂාංච යෝ නිරෝධ ඒවංවාදී මහාශ්‍රමණඃ । අඥානාච්චීයතේ කර්ම ජන්මනඃ කර්ම කාරණම් । ඥානාන්නචීයතේ කර්ම කර්මාභාවාන්න ජායතේ',
+      title: 'පටිච්චසමුප්පාද ගාථා',
+    },
     title: 'Paṭiccasamuppāda Gāthā',
   },
   path: new URL('', import.meta.url).pathname,
   sanskrit: {
-    children: 'ये धर्मा हेतु-प्रभवा हेतुं तेषां तथागत उवाच । तेषां च यो निरोध एवं वादी महाश्रमणः । अज्ञानाच्चीयते कर्म जन्मनः कर्म कारणम् । ज्ञानान्नचीयते कर्म कर्माभावान्न जायते',
+    children: 'ये धर्मा हेतुप्रभवा हेतुं तेषां तथागत उवाच । तेषांच यो निरोध एवंवादी महाश्रमणः । अज्ञानाच्चीयते कर्म जन्मनः कर्म कारणम् । ज्ञानान्नचीयते कर्म कर्माभावान्न जायते',
     title: 'प्रतीत्यसमुत्पाद गाथा',
   },
   tibetan: {
@@ -23,9 +27,9 @@ export default {
     title: 'Dependent Origination',
   },
   transliteration: {
-    children: 'ye dharmā hetuprabhavā hetuṃ teṣāṃ tathāgata uvāca । teṣāṃ ca yo nirodha evaṃ vādi mahāśramaṇaḥ । ajñānāc cīyate karma janmanaḥ karma kāraṇam । jñānān na cīyate karma karmābhāvān na jāyate',
+    children: 'ye dharmā hetuprabhavā hetuṃ teṣāṃ tathāgata uvāca । teṣāṃ-ca yo nirodha evaṃ vādi mahāśramaṇaḥ । ajñānāc cīyate karma janmanaḥ karma kāraṇam । jñānān na cīyate karma karmābhāvān na jāyate',
     repetition: 7,
-    speech: 'y dharma hei-tu-prabawa hei-tum t-samm tathagata uwaca, t-samm ca yo nirodha ewam wadi mahasramanah, ajnanac ciya-t karma jan-manah karma karanam, jnanan na ciya-t karma karmabawan na jayate',
+    speech: 'y dharma hei-tu-prabawa hei-tum t-samm tathagata uwaca, t-samm-ca yo nirodha ewam wadi mahasramanah, ajnanac ciya-t karma jan-manah karma karanam, jnanan na ciya-t karma karmabawan na jayate',
     title: 'Pratītyasamutpāda Gāthā',
   },
 };

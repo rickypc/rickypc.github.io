79ce785230d47851dbc12ce9e29b276e619b0bcf
Ricky Huang <rickypc@users.noreply.github.com>
2024-07-21 16:09:41 -0700

    Add phrase.

---
+++ docs/buddhism/phrases/_ekajati.js
@@ -0,0 +1,25 @@
+/*!
+ * All the code that follow is
+ * Copyright (c) 2015 - 2024 Richard Huang <rickypc@users.noreply.github.com>.
+ * All Rights Reserved. Not for reuse without permission.
+ */
+
+export default {
+  path: new URL('', import.meta.url).pathname,
+  sanskrit: {
+    children: 'समय होः । ॐ मम रुलु रुलु हूँ भ्यो हूँ । महा अमृत रक्त बलिंत पूजा होः धर्मधातु एवं',
+    title: 'एकजटी',
+  },
+  tibetan: {
+    children: 'ས་མ་ཡ་ཧོཿ། ཨོཾ་མ་མ་རུ་ལུ་རུ་ལུ་ཧཱུཾ་བྷྱོ་ཧཱུཾ། མ་ཧཱ་ཨ་མྲྀཏ་རཀྟ་བ་ལིཾ་ཏ་པཱུ་ཛ་ཧོཿདྷ་རྨ་དྷཱ་ཏུ་ཨེ་བཾ',
+    title: 'རལ་གཅིག་མ',
+  },
+  translation: {
+    title: 'One Plait Lady Primary',
+  },
+  transliteration: {
+    children: 'samaya hoḥ । oṃ mama rulu rulu hūṃ bhyō hūṃ । mahā amṛta rakta baliṃta pūja hoḥ dharmadhātu evaṃ',
+    speech: 'samaya hoh, oum mama ru-lu ru-lu houm bhyo houm, maha amrita rakta balimta puja hoh, dharmadhatu ewam',
+    title: 'Ekajaṭī Hṛdaya',
+  },
+};

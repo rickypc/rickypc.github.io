d386e62a97a8771e5e7c4ac4aaed847532244b6c
Ricky Huang <rickypc@users.noreply.github.com>
2024-07-23 15:58:08 -0700

    Add phrases.

---
+++ docs/buddhism/phrases/_vajra_dhara.js
@@ -0,0 +1,30 @@
+/*!
+ * All the code that follow is
+ * Copyright (c) 2015 - 2024 Richard Huang <rickypc@users.noreply.github.com>.
+ * All Rights Reserved. Not for reuse without permission.
+ */
+
+export default {
+  chinese: {
+    children: '嗡 啊 班雜達惹 悉地 吽 吽 · 嗡 班雜瑜伽女 吽 呸 梭哈',
+    title: '金剛總持',
+  },
+  path: new URL('', import.meta.url).pathname,
+  sanskrit: {
+    children: 'ॐ आः वज्रधर सिद्धि हूँ हूँ । ॐ वज्रयोगिनी हूँ फट् स्वाहा',
+    title: 'वज्रधर',
+  },
+  tibetan: {
+    children: 'ཨོཾ་ཨཱཿབཛྲ་དྷ་ར་སདྡྷེ་ཧཱུཾ་ཧཱུཾ། ༀ་བཛྲ་ཡོ་གི་ནཱི་ཧཱུཾ་ཕཊ་སྭཱཧཱ',
+    repeat: 79,
+    title: 'རྡོ་རྗེ་འཆང',
+  },
+  translation: {
+    title: 'Diamond Holder Primary',
+  },
+  transliteration: {
+    children: 'oṃ āḥ vajradhara siddhi hūṃ hūṃ । oṃ vajrayoginī hūṃ phaṭ svāhā',
+    speech: 'oum, ah, wajradhara siddhi houm houm, oum wajrayogini houm patt swaha',
+    title: 'Vajradhara Hṛdaya',
+  },
+};

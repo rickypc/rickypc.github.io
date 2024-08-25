c34efb32cf026409c546d9db8e41ebae7500aed2
Ricky Huang <rickypc@users.noreply.github.com>
2024-08-24 20:37:09 -0700

    Add phrases.

---
+++ docs/buddhism/phrases/_maitreya.js
@@ -0,0 +1,30 @@
+/*!
+ * All the code that follow is
+ * Copyright (c) 2015 - 2024 Richard Huang <rickypc@users.noreply.github.com>.
+ * All Rights Reserved. Not for reuse without permission.
+ */
+
+export default {
+  chinese: {
+    children: '嗡 莫嘿 莫嘿 馬哈莫嘿 梭哈 · 嗡 牟尼 牟尼 思嗎拉 梭哈',
+    title: '彌勒佛',
+  },
+  path: new URL('', import.meta.url).pathname,
+  sanskrit: {
+    children: 'ॐ मोहि मोहि महामोहि स्वाहा । ॐ मुनि मुनि स्मरा स्वाहा',
+    title: 'मैत्रेय',
+  },
+  tibetan: {
+    children: 'ཨོཾ་མོ་ཧི་མོ་ཧི་མ་ཧཱ་མོ་ཧི་སྭཱ་ཧཱ། ཨོཾ་མུ་ནི་མུ་ནི་སྨ་རཱ་སྭཱ་ཧཱ',
+    repeat: {
+      roll: 77,
+      wheel: 75,
+    },
+    title: 'བྱམས་པ',
+  },
+  transliteration: {
+    children: 'oṃ mohi mohi mahāmohi svāhā । oṃ muni muni smarā svāhā',
+    speech: 'oum mohi mohi maha mohi swaha, oum muni muni smara swaha',
+    title: 'Maitreya Hṛdaya',
+  },
+};
---
+++ docs/buddhism/phrases/_usnisa_sitatapatra.js
@@ -0,0 +1,31 @@
+/*!
+ * All the code that follow is
+ * Copyright (c) 2015 - 2024 Richard Huang <rickypc@users.noreply.github.com>.
+ * All Rights Reserved. Not for reuse without permission.
+ */
+
+export default {
+  chinese: {
+    children: '嗡 灑兒哇 咑他噶咑 烏殊尼沙 施達打巴遮 吽帕 吽 嘛嘛 吽 尼 梭哈',
+    title: '大白伞盖佛母',
+  },
+  path: new URL('', import.meta.url).pathname,
+  sanskrit: {
+    children: 'ॐ सर्व तथागत ऊष्णीष सितातपतत्रे हूँ फट हूँ मम हूँ नि स्वाहा',
+    title: 'ऊष्णीष सितातपत्रा',
+  },
+  tibetan: {
+    children: 'ཨོཾ་སརྦ་ཏ་ཐཱ་ག་ཏ་ཨཱུ་ཥྞཱི་ཥ་སི་ཏཱ་ཏ་པ་ཏྲེ་ཧཱུྃ་ཕཊ་ཧཱུྃ་མ་མ་ཧཱུྃ་ནི་སྭཱ་ཧཱ',
+    repeat: {
+      roll: 67,
+      wheel: 65,
+    },
+    title: 'གདུགས་དཀར་མོ',
+  },
+  transliteration: {
+    children: 'oṃ sarva tathāgata uṣṇīṣa sitātapatre hūṃ phaṭ hūṃ mama hūṃ ni svāhā',
+    speech: 'oum sarwa tathagata, usnisa sitatapatre, houm patt, houm mama, houm ni, swaha',
+    // White Parasol Goddess.
+    title: 'Uṣṇīṣa Sitātapatrā Hṛdaya',
+  },
+};
